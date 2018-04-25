a3abba9a215148e9d4229bb3404430a389697717
Denys Boiko <denys.boiko@gmail.com>
2018-04-26 00:21:46 +0300

    patterns

--- reach/urls.py
+++ reach/urls.py
@@ -3,7 +3,8 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from cms.sitemaps import CMSSitemap
 from django.conf import settings
-from django.conf.urls import include, url, patterns
+from django.conf.urls import include, url
+# from django.conf.urls.defaults import *
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
